f253b6d601bfe8099c401abe8895e543b8296492
FelipeEnne <felipeenne@gmail.com>
2020-08-29 15:38:39 -0300

    problem array manipulation

---
+++ Array Manipulation/array-manipulation.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the arrayManipulation function below.
+function arrayManipulation(n, queries) {
+    let result = new Array(n).fill(0)
+    // console.log(result);
+    // console.log(queries);
+
+    for(const query of queries){
+        
+        const a = query[0]-1;
+        const b = query[1];
+        const k = query[2];
+        result[a] += k;
+        result[b] -= k
+        //console.log(query)
+        //console.log(result)
+    }
+
+    //console.log(result)
+
+    let max = 0
+    let run = 0
+
+    for(const i of result){
+        run += i
+        if(run > max){
+            max = run
+        }
+    }
+    return max
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nm = readLine().split(' ');
+
+    const n = parseInt(nm[0], 10);
+
+    const m = parseInt(nm[1], 10);
+
+    let queries = Array(m);
+
+    for (let i = 0; i < m; i++) {
+        queries[i] = readLine().split(' ').map(queriesTemp => parseInt(queriesTemp, 10));
+    }
+
+    let result = arrayManipulation(n, queries);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
