4c2fdf1077f9d22ce9eb8a8faf6c72d2f1fe6987
Felipe Enne Mendes Ribeiro <felipeenne@gmail.com>
2021-01-12 07:47:48 -0300

    Create Sales by Match.js

---
+++ Sales by Match.js
@@ -0,0 +1,56 @@
+'use strict';
+
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+    let mapSocks = new Map();
+    let result = 0;
+    
+    for(let i = 0; i < ar.length; i++) {        
+        if(mapSocks.get(ar[i]) && mapSocks.get(ar[i]) > 0) {
+            mapSocks.set(ar[i], mapSocks.get(ar[i])-1)
+            result++
+        } else {
+            mapSocks.set(ar[i], 1)
+        }
+    }
+    return result;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = sockMerchant(n, ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
