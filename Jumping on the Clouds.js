3a384f5a25ccfd5df3cd790bcab7c70a4e5d17c8
Felipe Enne Mendes Ribeiro <felipeenne@gmail.com>
2021-01-28 21:48:26 -0300

    Create Jumping on the Clouds.js

---
+++ Jumping on the Clouds.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the jumpingOnClouds function below.
+function jumpingOnClouds(c) {
+
+    let result = 0;
+    for(let i = 0; i <= c.length-1; i++ ){
+        if(i+2 <= c.length-1 && c[i+2] !== 1) {
+            i++;
+        }
+       if(i === c.length-1) return result;
+        result += 1;
+    }
+    return result;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const c = readLine().split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    let result = jumpingOnClouds(c);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
