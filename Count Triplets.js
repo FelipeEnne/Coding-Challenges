55d476715adca4b8912ea41e1ea9ce8b3200e0bf
Felipe Enne Mendes Ribeiro <felipeenne@gmail.com>
2021-02-19 08:24:23 -0300

    Create Count Triplets.js

---
+++ Count Triplets.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the countTriplets function below.
+function countTriplets(arr, r) {
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nr = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(nr[0], 10);
+
+    const r = parseInt(nr[1], 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const ans = countTriplets(arr, r);
+
+    ws.write(ans + '\n');
+
+    ws.end();
+}
