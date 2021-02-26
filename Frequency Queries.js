9f1335e15bd8f9644498259c58815972b974f261
Felipe Enne Mendes Ribeiro <felipeenne@gmail.com>
2021-02-26 10:20:43 -0300

    Create Frequency Queries.js

---
+++ Frequency Queries.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the freqQuery function below.
+function freqQuery(queries) {
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine().trim(), 10);
+
+    let queries = Array(q);
+
+    for (let i = 0; i < q; i++) {
+        queries[i] = readLine().replace(/\s+$/g, '').split(' ').map(queriesTemp => parseInt(queriesTemp, 10));
+    }
+
+    const ans = freqQuery(queries);
+
+    ws.write(ans.join('\n') + '\n');
+
+    ws.end();
+}
