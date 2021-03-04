50a6c20abc35c67f93f35bae8233583ccedc923f
Felipe Enne Mendes Ribeiro <felipeenne@gmail.com>
2021-03-04 08:26:03 -0300

    Create Sorting: Bubble Sort.js

---
+++ Sorting: Bubble Sort.js
@@ -0,0 +1,37 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the countSwaps function below.
+function countSwaps(a) {
+
+
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    countSwaps(a);
+}
