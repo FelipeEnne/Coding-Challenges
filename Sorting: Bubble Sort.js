f56cd2f1ed64283d1961549fdea78103e542381f
Felipe Enne Mendes Ribeiro <felipeenne@gmail.com>
2021-03-05 08:42:27 -0300

    Update Sorting: Bubble Sort.js

--- Sorting: Bubble Sort.js
+++ Sorting: Bubble Sort.js
@@ -24,8 +24,24 @@ function readLine() {
 
 // Complete the countSwaps function below.
 function countSwaps(a) {
-
-
+    let countSwap = 0;
+    for (let i = 0; i < a.length; i++) {
+    
+        for (let j = 0; j < a.length - 1; j++) {
+            // Swap adjacent elements if they are in decreasing order
+            if (a[j] > a[j + 1]) {
+                const temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+                countSwap ++;
+            }
+        }
+        
+    }
+    
+    console.log(`Array is sorted in ${countSwap} swaps.`)
+    console.log(`First Element: ${a[0]}`)
+    console.log(`Last Element: ${a[a.length - 1]}`)
 }
 
 function main() {
