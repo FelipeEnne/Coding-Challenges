100cb90f9b0925412ad417dab3f8a71c83897894
Felipe Enne Mendes Ribeiro <felipeenne@gmail.com>
2021-03-09 08:59:42 -0300

    Update Sorting: Comparator.cpp

--- Sorting: Comparator.cpp
+++ Sorting: Comparator.cpp
@@ -14,7 +14,40 @@ class Checker{
 public:
   	// complete this method
     static int comparator(Player a, Player b)  {
-	
+        if(a.score < b.score) {
+            return -1;
+        } 
+    
+        if(a.score > b.score) {
+            return 1;
+        }
+        
+        if(a.score == b.score) {
+            int count = 0;
+            
+            if(a.name[count] < b.name[count]) {
+                return 1;
+            }
+            
+            if(a.name[count] > b.name[count]) {
+                return -1;
+            }
+            
+            count ++;
+            while(a.name[count] == b.name[count]) {
+                if(a.name[count] < b.name[count]) {
+                    return 1;
+                }
+            
+                if(a.name[count] > b.name[count]) {
+                    return -1;
+                }
+                count ++;
+            }
+            return 0;
+        } 
+        
+        return 0;
     }
 };
 
