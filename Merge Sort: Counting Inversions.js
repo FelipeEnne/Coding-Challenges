85dc6bbf16066616d68cb2b5655ca49b453b41c4
Felipe Enne Mendes Ribeiro <felipeenne@gmail.com>
2021-03-31 12:04:41 -0300

    Update Merge Sort: Counting Inversions.js

--- Merge Sort: Counting Inversions.js
+++ Merge Sort: Counting Inversions.js
@@ -24,41 +24,42 @@ function readLine() {
     return inputString[currentLine++];
 }
 
-function swap(arr1, arr2, count) {
-    let i = 0;
-    let j = 0;
-    let mergedArr = [];
-    let remainder;
-        
-    while (i !== arr1.length && j !== arr2.length) {
-        if (arr1[i] < arr2[j] || arr1[i] === arr2[j]) {
-            mergedArr.push(arr1[i]);
-            i++;
-        } else {
-            mergedArr.push(arr2[j]);
-            count += (arr1.length - i);
-            j++;
-        }
-    }
-    remainder = i >= j ? arr2.slice(j) : arr1.slice(i);
-    return mergedArr.concat(remainder);
-}
-
-function mergeSort(arr, count) {
-    if (arr.length <= 1) return arr;
-    let middle = Math.floor(arr.length / 2);
-
-    let left = mergeSort(arr.slice(0, middle))
-    let right = mergeSort(arr.slice(middle, arr.length))
-
-    return swap(left, right, count);
-}
-    
 // Complete the countInversions function below.
 function countInversions(arr) {
     let count = 0;
+    
+    function swap(arr1, arr2) {
+        let i = 0;
+        let j = 0;
+        
+        let mergedArr = [];
+        let remainder;
+        
+        while (i !== arr1.length && j !== arr2.length) {
+            if (arr1[i] < arr2[j] || arr1[i] === arr2[j]) {
+                mergedArr.push(arr1[i]);
+                i++;
+            } else {
+                mergedArr.push(arr2[j]);
+                count += (arr1.length - i);
+                j++;
+            }
+        }
+        remainder = i >= j ? arr2.slice(j) : arr1.slice(i);
+        return mergedArr.concat(remainder);
+    }
 
-    mergeSort(arr, count);
+    function mergeSort(arr) {
+        if (arr.length <= 1) return arr;
+        let middle = Math.floor(arr.length / 2);
+
+        let left = mergeSort(arr.slice(0, middle))
+        let right = mergeSort(arr.slice(middle, arr.length))
+
+        return swap(left, right);
+    }
+    
+    mergeSort(arr);
     
     return count;
 }
