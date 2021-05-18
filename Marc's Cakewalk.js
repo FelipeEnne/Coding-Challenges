8b1a7e9a4f2de224d227f08f450cd55533214026
Felipe Enne Mendes Ribeiro <felipeenne@gmail.com>
2021-05-18 08:44:56 -0300

    Create Marc's Cakewalk.js

---
+++ Marc's Cakewalk.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'marcsCakewalk' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts INTEGER_ARRAY calorie as parameter.
+ */
+
+function marcsCakewalk(calorie) {
+    // Write your code here
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const calorie = readLine().replace(/\s+$/g, '').split(' ').map(calorieTemp => parseInt(calorieTemp, 10));
+
+    const result = marcsCakewalk(calorie);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
