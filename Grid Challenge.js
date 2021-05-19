4966540234d0ff0feec824af7768762bcd2bb0c5
Felipe Enne Mendes Ribeiro <felipeenne@gmail.com>
2021-05-19 11:22:15 -0300

    Create Grid Challenge.js

---
+++ Grid Challenge.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'gridChallenge' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING_ARRAY grid as parameter.
+ */
+
+function gridChallenge(grid) {
+    // Write your code here
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine().trim(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const n = parseInt(readLine().trim(), 10);
+
+        let grid = [];
+
+        for (let i = 0; i < n; i++) {
+            const gridItem = readLine();
+            grid.push(gridItem);
+        }
+
+        const result = gridChallenge(grid);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
