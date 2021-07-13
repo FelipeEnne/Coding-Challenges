e8bb2b9d69cc9322e4d1329e8934413e2bbe5082
Felipe Enne Mendes Ribeiro <felipeenne@gmail.com>
2021-07-13 08:26:41 -0300

    Update Pairs.js

--- Pairs.js
+++ Pairs.js
@@ -38,27 +38,18 @@ function pairs(k, arr) {
     let result = 0;
     
     for(let i = 0; arr.length > i; i ++){
-        let value = arr[i] > k ? Math.abs(arr[i]-k) : arr[i]+k;
         
-        if(mapArr[value]) {
-            mapArr[value] += 1;
-        } else {
-             mapArr[value] = 1;
-        }
-        // console.log({mapArr, value});
-    };
-    console.log({result, mapArr});
-    
-    for(let i = 0; arr.length > i; i ++){
-        if(mapArr[arr[i]] && mapArr[arr[i]] > 0) { 
-            let value = arr[i] > k ? Math.abs(arr[i]-k) : arr[i]+k;
-           
+        
+        if(mapArr[arr[i]]) {
             result += mapArr[arr[i]];
-            mapArr[arr[i]] -= 1;
-            mapArr[value] -= 1;
-        } 
-        console.log({mapArr});
+        } else {
+            let value = arr[i] > k ? Math.abs(arr[i]-k) : arr[i]+k;
+            mapArr[value] = mapArr[value] ? mapArr[value]+1 : 1;
+        }
+        
+        console.log({mapArr, result});
     };
+
     
     return result;
 }
