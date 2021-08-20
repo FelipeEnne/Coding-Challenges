f5f5362251650a7a7be82f09ac84dc0f6b6f8aab
Felipe Enne Mendes Ribeiro <felipeenne@gmail.com>
2021-08-20 08:45:39 -0300

    Create Abbreviation.js

---
+++ Abbreviation.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'abbreviation' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING a
+ *  2. STRING b
+ */
+
+function abbreviation(a, b) {
+    // Write your code here
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine().trim(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const a = readLine();
+
+        const b = readLine();
+
+        const result = abbreviation(a, b);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
