f0b6828ca0de79c6cfc2d942faae868bbb500da6
Felipe Enne Mendes Ribeiro <felipeenne@gmail.com>
2021-10-01 10:14:20 -0300

    Update Largest Rectangle.js

--- Largest Rectangle.js
+++ Largest Rectangle.js
@@ -30,23 +30,23 @@ function readLine() {
  */
 
 function largestRectangle(h) {
-    // Write your code here
-    let h1 = h.length;
-    let l1 = h[0];
+    const heights = [...h, 0]
+    let area = 0
+    let i = 0
+    let positions = []
     
-    let result = h1*l1;
-
-
-    for(let i = 0; h.length > i; i++){
-        h.shift();
-        h1 = h.length;
-        l1 = Math.min(...h);
-    
-        if(h1*l1 > result) result = h1*l1;
-
+    while (i < heights.length) {
+        if (!positions.length || heights[positions[positions.length-1]] <= heights[i]) {
+            positions.push(i++)
+        } else {
+            const top = positions.pop()
+            const lastPosition = positions[positions.length-1]
+            const buildingHeight = heights[top]
+            const numberOfBuildings = positions.length ? (i - lastPosition - 1) : i
+            area = Math.max(area, buildingHeight * numberOfBuildings)
+        }
     }
-    
-    return result;
+    return area
 }
 
 function main() {
