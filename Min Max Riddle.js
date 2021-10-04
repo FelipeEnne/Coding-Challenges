d580611b48390a0eb901964dab3cf064dadde69f
Felipe Enne Mendes Ribeiro <felipeenne@gmail.com>
2021-10-04 08:46:21 -0300

    Create Min Max Riddle.js

---
+++ Min Max Riddle.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the solve function below.
+function solve(arr) {
+    // solve here
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const res = riddle(arr);
+
+    ws.write(res.join(' ') + '\n');
+
+    ws.end();
+}
