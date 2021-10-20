bca823577ba636528698143d2a320ac7c7c23a0f
Felipe Enne Mendes Ribeiro <felipeenne@gmail.com>
2021-10-20 07:15:43 -0300

    Update Castle on the Grid.js

--- Castle on the Grid.js
+++ Castle on the Grid.js
@@ -62,6 +62,35 @@ function minimumMoves(grid, startX, startY, goalX, goalY) {
         queue.push(newCell)
         return newCell
     }
+    
+    while (queue.length > 0) {
+        const currentCell = queue.shift()
+        if (
+            currentCell.position.x === goalX &&
+            currentCell.position.y === goalY
+        ) {
+            return currentCell.moves
+        }
+        const { position } = currentCell
+        const moves = currentCell.moves + 1
+        // LEFT
+        for (let y = position.y - 1; y >= 0; y--) {
+            if (!markCellAsVisited(position.x, y, moves, currentCell)) break
+        }
+        // TOP
+        for (let x = position.x - 1; x >= 0; x--) {
+            if (!markCellAsVisited(x, position.y, moves, currentCell)) break
+        }
+        // RIGHT
+        for (let y = position.y + 1; y < grid.length; y++) {
+            if (!markCellAsVisited(position.x, y, moves, currentCell)) break
+        }
+        // BOTTOM
+        for (let x = position.x + 1; x < grid.length; x++) {
+            if (!markCellAsVisited(x, position.y, moves, currentCell)) break
+        }
+    }
+    return 0
 }
 
 function main() {
