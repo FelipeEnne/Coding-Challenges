bb42eac168e924fa77bff48efcecebb39288f39f
Felipe Enne Mendes Ribeiro <felipeenne@gmail.com>
2021-10-21 07:24:02 -0300

    Create Poisonous Plants.js

---
+++ Poisonous Plants.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'poisonousPlants' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY p as parameter.
+ */
+
+function poisonousPlants(p) {
+    // Write your code here
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const p = readLine().replace(/\s+$/g, '').split(' ').map(pTemp => parseInt(pTemp, 10));
+
+    const result = poisonousPlants(p);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
