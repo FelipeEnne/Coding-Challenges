9044dc55ee12dfc450625ffbd7c94ab447919f17
Felipe Enne Mendes Ribeiro <felipeenne@gmail.com>
2021-10-28 07:25:57 -0300

    Update Poisonous Plants.js

--- Poisonous Plants.js
+++ Poisonous Plants.js
@@ -34,9 +34,23 @@ function poisonousPlants(p) {
     var stack = [];
      for (let i = 0; i < p.length; i++){
         let daysAlive = 0;
-     
-     
+         
+        while(stack.length > 0 && p[i] <= stack[stack.length - 1].plant)
+            daysAlive = Math.max(daysAlive, stack.pop().days); 
+        
+         if (stack.length === 0) 
+            daysAlive = 0;
+          else 
+            daysAlive += 1;
+         
+         maxDaysAlive = Math.max(maxDaysAlive, daysAlive);
+         stack.push({ 
+            plant: p[i],
+            days : daysAlive
+        });
      }
+    
+    return maxDaysAlive;
 }
 
 function main() {
