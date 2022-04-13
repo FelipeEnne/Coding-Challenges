6cc53cf20aeccaa6b97f3b7c84d125be30e0c7f2
FelipeEnne <felipeenne@gmail.com>
2022-04-13 12:02:37 -0300

    finish shortest reach in a graph

--- Graphs (HanckerRank)/Shortest Reach in a Graph.js
+++ Graphs (HanckerRank)/Shortest Reach in a Graph.js
@@ -1,91 +1,89 @@
 function processData(input) {
-    input = input.split("\n");
-    const numberOfQueries = parseInt(input.shift());
-    
-    let arrIdx = 0;
-    
-    for (let i = 0; i < numberOfQueries; i++) {
-        let arrNodesEdges = input[arrIdx++].split(" ").map(Number);
+  input = input.split("\n");
+  const numberOfQueries = parseInt(input.shift());
 
-        const numberOfNodes = arrNodesEdges[0];
-        const numberOfEdges = arrNodesEdges[1];
-        
-        const nodesArr = [];
-        for (let w = 0; w < numberOfEdges; w++) {
-            let node = input[arrIdx++].split(" ").map(Number);
-            const fromNode = node[0];
-            const toNode = node[1];
+  let arrIdx = 0;
 
-            if (!nodesArr[fromNode]) {
-                nodesArr[fromNode] = [];
-            }
-            if (!nodesArr[toNode]) {
-                nodesArr[toNode] = [];
-            }
-            if (fromNode > numberOfNodes || toNode > numberOfNodes) {
-                continue;
-            }
-            nodesArr[fromNode].push(toNode);
-            nodesArr[toNode].push(fromNode);
-        }
+  for (let i = 0; i < numberOfQueries; i++) {
+    let arrNodesEdges = input[arrIdx++].split(" ").map(Number);
 
+    const numberOfNodes = arrNodesEdges[0];
+    const numberOfEdges = arrNodesEdges[1];
 
-        const root = parseInt(input[arrIdx++]);
-        
-        const passedOver = [];
-        processGraph(nodesArr, root, passedOver);
-        //console.log(passedOver)
-        
-        const res = [];
-        for (let j = 1; j <= numberOfNodes; j++) {
-            if (j == root) {
-                continue;
-            }
+    const nodesArr = [];
+    for (let w = 0; w < numberOfEdges; w++) {
+      let node = input[arrIdx++].split(" ").map(Number);
+      const fromNode = node[0];
+      const toNode = node[1];
 
-            if (!passedOver[j]) {
-                res.push(-1);
-                continue;
-            }
-
-            res.push(passedOver[j]);
-        }
-        console.log(res.join(" "));
+      if (!nodesArr[fromNode]) {
+        nodesArr[fromNode] = [];
+      }
+      if (!nodesArr[toNode]) {
+        nodesArr[toNode] = [];
+      }
+      if (fromNode > numberOfNodes || toNode > numberOfNodes) {
+        continue;
+      }
+      nodesArr[fromNode].push(toNode);
+      nodesArr[toNode].push(fromNode);
     }
+
+    const root = parseInt(input[arrIdx++]);
+
+    const passedOver = [];
+    processGraph(nodesArr, root, passedOver);
+
+    const res = [];
+    for (let j = 1; j <= numberOfNodes; j++) {
+      if (j == root) {
+        continue;
+      }
+
+      if (!passedOver[j]) {
+        res.push(-1);
+        continue;
+      }
+
+      res.push(passedOver[j]);
+    }
+    console.log(res.join(" "));
+  }
 }
 
 function processGraph(nodesArr, root, passedOver) {
-    const queue = [];
-    queue.push(root);
-    passedOver[root] = 0;
+  const queue = [];
+  queue.push(root);
+  passedOver[root] = 0;
 
-    while (queue.length > 0) {
-        const nodeIdx = queue.shift();
+  while (queue.length > 0) {
+    const nodeIdx = queue.shift();
 
-        const nodeConnections = nodesArr[nodeIdx];
-        if (!nodeConnections) {
-            continue;
-        }
-
-        const distanceFromRoot = passedOver[nodeIdx];
-        for (let i = 0; i < nodeConnections.length; i++) {
-            const connIdx = nodeConnections[i];
-            if (passedOver[connIdx]) {
-                continue;
-            }
-
-            passedOver[connIdx] = distanceFromRoot + 6;
-            queue.push(connIdx);
-        }
+    const nodeConnections = nodesArr[nodeIdx];
+    if (!nodeConnections) {
+      continue;
     }
+
+    const distanceFromRoot = passedOver[nodeIdx];
+    for (let i = 0; i < nodeConnections.length; i++) {
+      const connIdx = nodeConnections[i];
+      if (passedOver[connIdx]) {
+        continue;
+      }
+
+      passedOver[connIdx] = distanceFromRoot + 6;
+      queue.push(connIdx);
+    }
+  }
 }
 
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
 _input = "";
 process.stdin.on("data", function (input) {
-    _input += input;
+  _input += input;
 });
 
 process.stdin.on("end", function () {
-   processData(_input);
+  processData(_input);
 });
