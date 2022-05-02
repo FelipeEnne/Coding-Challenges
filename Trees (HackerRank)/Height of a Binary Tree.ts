1552621b9b21de868dc39b9b8a8adcebbc3d5d97
FelipeEnne <felipeenne@gmail.com>
2022-05-02 19:50:12 -0300

    continue Tree height of a binary tree

--- Trees (HackerRank)/Height of a Binary Tree.ts
+++ Trees (HackerRank)/Height of a Binary Tree.ts
@@ -1,24 +1,27 @@
-"use strict";
+'use strict';
 
 process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-let inputString: string = "";
+process.stdin.setEncoding('utf-8');
+let inputString: string = '';
 let inputLines: string[] = [];
 let currentLine: number = 0;
-process.stdin.on("data", function (inputStdin: string): void {
-  inputString += inputStdin;
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
 });
 
-process.stdin.on("end", function (): void {
-  inputLines = inputString.split("\n");
-  inputString = "";
-  main();
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+    main();
 });
 
 function readLine(): string {
-  return inputLines[currentLine++];
+    return inputLines[currentLine++];
 }
 
 function main() {
-  // Enter your code here
+    // Enter your code here
+    const numberOfInputs = parseInt(inputLines[0])
+    const numbers = inputLines[1].split(' ').map(e => parseInt(e))
+    console.log({numberOfInputs, numbers})
 }
