d7658e5741e2b2abda61a84bcb0a1ced7c58f560
FelipeEnne <felipeenne@gmail.com>
2022-05-03 19:20:06 -0300

    continue Tree: Height of a Binary Tree

--- Trees (HackerRank)/Height of a Binary Tree.ts
+++ Trees (HackerRank)/Height of a Binary Tree.ts
@@ -1,27 +1,34 @@
-'use strict';
+"use strict";
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-let inputString: string = '';
+process.stdin.setEncoding("utf-8");
+let inputString: string = "";
 let inputLines: string[] = [];
 let currentLine: number = 0;
-process.stdin.on('data', function(inputStdin: string): void {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin: string): void {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function(): void {
-    inputLines = inputString.split('\n');
-    inputString = '';
-    main();
+process.stdin.on("end", function (): void {
+  inputLines = inputString.split("\n");
+  inputString = "";
+  main();
 });
 
 function readLine(): string {
-    return inputLines[currentLine++];
+  return inputLines[currentLine++];
 }
 
 function main() {
-    // Enter your code here
-    const numberOfInputs = parseInt(inputLines[0])
-    const numbers = inputLines[1].split(' ').map(e => parseInt(e))
-    console.log({numberOfInputs, numbers})
+  // Enter your code here
+  const numberOfInputs = parseInt(inputLines[0]);
+  const numbers = inputLines[1].split(" ").map((e) => parseInt(e));
+
+  let result = 0;
+
+  for (let i = 0; i < numberOfInputs; i++) {
+    console.log(numbers[i]);
+  }
+
+  console.log({ numberOfInputs, numbers });
 }
