45d87d418aba3f0235e931371937da93a94ec2f0
Felipe Enne <felipeenne@gmail.com>
2022-08-11 19:17:46 -0300

    Reverse a doubly linked list

--- Linked Lists (HackRank)/Reverse a doubly linked list.js
+++ Linked Lists (HackRank)/Reverse a doubly linked list.js
@@ -1,67 +1,68 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', _ => {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .replace(/\s*$/, "")
+    .split("\n")
+    .map((str) => str.replace(/\s*$/, ""));
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 const DoublyLinkedListNode = class {
-    constructor(nodeData) {
-        this.data = nodeData;
-        this.next = null;
-        this.prev = null;
-    }
+  constructor(nodeData) {
+    this.data = nodeData;
+    this.next = null;
+    this.prev = null;
+  }
 };
 
 const DoublyLinkedList = class {
-    constructor() {
-        this.head = null;
-        this.tail = null;
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  insertNode(nodeData) {
+    let node = new DoublyLinkedListNode(nodeData);
+
+    if (this.head == null) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
     }
 
-    insertNode(nodeData) {
-        let node = new DoublyLinkedListNode(nodeData);
-
-        if (this.head == null) {
-            this.head = node;
-        } else {
-            this.tail.next = node;
-            node.prev = this.tail;
-        }
-
-        this.tail = node;
-    }
+    this.tail = node;
+  }
 };
 
 function printDoublyLinkedList(node, sep, ws) {
-    while (node != null) {
-        ws.write(String(node.data));
+  while (node != null) {
+    ws.write(String(node.data));
 
-        node = node.next;
+    node = node.next;
 
-        if (node != null) {
-            ws.write(sep);
-        }
+    if (node != null) {
+      ws.write(sep);
     }
+  }
 }
 
 /*
@@ -83,8 +84,42 @@ function printDoublyLinkedList(node, sep, ws) {
  */
 
 function reverse(llist) {
-    // Write your code here
+  // Write your code here
+  let pointer = llist;
+  while (pointer) {
+    let pointerPrev = pointer.prev;
+    let pointerNext = pointer.next;
+    pointer.next = pointerPrev;
+    pointer.prev = pointerNext;
+    if (!pointerNext) {
+      break;
+    }
+    pointer = pointerNext;
+  }
 
+  return pointer;
 }
 
 function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const t = parseInt(readLine(), 10);
+
+  for (let tItr = 0; tItr < t; tItr++) {
+    const llistCount = parseInt(readLine(), 10);
+
+    let llist = new DoublyLinkedList();
+
+    for (let i = 0; i < llistCount; i++) {
+      const llistItem = parseInt(readLine(), 10);
+      llist.insertNode(llistItem);
+    }
+
+    let llist1 = reverse(llist.head);
+
+    printDoublyLinkedList(llist1, " ", ws);
+    ws.write("\n");
+  }
+
+  ws.end();
+}
