5de5ca16d87dda749ea29f6b2c74dd56f7ea22da
Felipe Enne <felipeenne@gmail.com>
2022-08-25 18:56:42 -0300

    start Recursion Davis Staircase

---
+++ Recursion and Backtracking/Recursion Davis' Staircase.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'stepPerms' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+function stepPerms(n) {
+  // Write your code here
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const s = parseInt(readLine().trim(), 10);
+
+  for (let sItr = 0; sItr < s; sItr++) {
+    const n = parseInt(readLine().trim(), 10);
+
+    const res = stepPerms(n);
+
+    ws.write(res + "\n");
+  }
+
+  ws.end();
+}
