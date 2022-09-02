75834122cda46043829ab3488d26630eb85da579
Felipe Enne <felipeenne@gmail.com>
2022-09-02 19:15:36 -0300

    Crossword Puzzle

--- Recursion and Backtracking/Crossword Puzzle.js
+++ Recursion and Backtracking/Crossword Puzzle.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -32,25 +32,26 @@ function readLine() {
  */
 
 function crosswordPuzzle(crossword, words) {
-    // Write your code here
+  hints = words.split(";");
 
+  let options = [crossword];
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    let crossword = [];
+  let crossword = [];
 
-    for (let i = 0; i < 10; i++) {
-        const crosswordItem = readLine();
-        crossword.push(crosswordItem);
-    }
+  for (let i = 0; i < 10; i++) {
+    const crosswordItem = readLine();
+    crossword.push(crosswordItem);
+  }
 
-    const words = readLine();
+  const words = readLine();
 
-    const result = crosswordPuzzle(crossword, words);
+  const result = crosswordPuzzle(crossword, words);
 
-    ws.write(result.join('\n') + '\n');
+  ws.write(result.join("\n") + "\n");
 
-    ws.end();
+  ws.end();
 }
