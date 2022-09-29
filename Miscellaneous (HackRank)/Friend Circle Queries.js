01be2e902c2834e6774628218dcdbfa979afdaa1
Felipe Enne <felipeenne@gmail.com>
2022-09-29 19:10:35 -0300

    Friend Circle Queries

---
+++ Miscellaneous (HackRank)/Friend Circle Queries.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString
+    .replace(/\s*$/, "")
+    .split("\n")
+    .map((str) => str.replace(/\s*$/, ""));
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+// Complete the maxCircle function below.
+function maxCircle(queries) {}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const q = parseInt(readLine(), 10);
+
+  let queries = Array(q);
+
+  for (let i = 0; i < q; i++) {
+    queries[i] = readLine()
+      .split(" ")
+      .map((queriesTemp) => parseInt(queriesTemp, 10));
+  }
+
+  const ans = maxCircle(queries);
+
+  ws.write(ans.join("\n") + "\n");
+
+  ws.end();
+}
