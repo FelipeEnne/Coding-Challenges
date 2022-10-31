79f0c36f72d09878ed4d82cb4306c7bead1aca3b
Felipe Enne <felipeenne@gmail.com>
2022-10-31 19:01:49 -0300

    Arrays: Left Rotation

--- Arrays (HackerRank)/2D Array - DS/2D Array - DS.js
+++ Arrays (HackerRank)/2D Array - DS/2D Array - DS.js
@@ -1,58 +1,68 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', _ => {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .replace(/\s*$/, "")
+    .split("\n")
+    .map((str) => str.replace(/\s*$/, ""));
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 // Complete the hourglassSum function below.
 function hourglassSum(arr) {
-    let result = -1000;
-    for(let i = 0; i <= arr.length - 1; i++) {
-        for(let j = 0; j <= arr.length -1; j++) {
-          if(i < arr.length - 2 && j < arr.length - 2){  
-              let sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
-              if(sum > result) {
-                  result = sum
-              }
-          }
+  let result = -1000;
+  for (let i = 0; i <= arr.length - 1; i++) {
+    for (let j = 0; j <= arr.length - 1; j++) {
+      if (i < arr.length - 2 && j < arr.length - 2) {
+        let sum =
+          arr[i][j] +
+          arr[i][j + 1] +
+          arr[i][j + 2] +
+          arr[i + 1][j + 1] +
+          arr[i + 2][j] +
+          arr[i + 2][j + 1] +
+          arr[i + 2][j + 2];
+        if (sum > result) {
+          result = sum;
         }
+      }
     }
-    
-    return result;
+  }
+
+  return result;
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    let arr = Array(6);
+  let arr = Array(6);
 
-    for (let i = 0; i < 6; i++) {
-        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
-    }
+  for (let i = 0; i < 6; i++) {
+    arr[i] = readLine()
+      .split(" ")
+      .map((arrTemp) => parseInt(arrTemp, 10));
+  }
 
-    let result = hourglassSum(arr);
+  let result = hourglassSum(arr);
 
-    ws.write(result + "\n");
+  ws.write(result + "\n");
 
-    ws.end();
+  ws.end();
 }
