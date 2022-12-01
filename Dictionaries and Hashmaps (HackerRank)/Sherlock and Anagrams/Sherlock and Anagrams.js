63c7a70220b9b32aa598b26fcb685eb1eac9cb6b
Felipe Enne <felipeenne@gmail.com>
2022-11-30 21:18:29 -0300

    add description to Sherlock and Anagrams

--- Dictionaries and Hashmaps (HackerRank)/Sherlock and Anagrams/Sherlock and Anagrams.js
+++ Dictionaries and Hashmaps (HackerRank)/Sherlock and Anagrams/Sherlock and Anagrams.js
@@ -1,70 +1,70 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', _ => {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .replace(/\s*$/, "")
+    .split("\n")
+    .map((str) => str.replace(/\s*$/, ""));
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
-const sortString = (string)  => string.split('').sort().join('');
+const sortString = (string) => string.split("").sort().join("");
 
 // Complete the sherlockAndAnagrams function below.
 function sherlockAndAnagrams(s) {
-   
-    let stringAnagramMap = {};
-    let result = 0;
-    
-    for(let i = 0; i <= s.length; i++) {
-        for(let j = 0; j < i; j++) {
-            const sliceIJ = sortString(s.slice(j,i));
-            if(i!== j && sliceIJ !== '') {
-                if(!!stringAnagramMap[sliceIJ]) { 
-                    stringAnagramMap[sliceIJ] += 1;
-                    result += stringAnagramMap[sliceIJ] - 1;
-                    //console.log(result)
-                } else if(i!== j){
-                    stringAnagramMap[sliceIJ] = 1;
-                }
-            }
-            
-            //console.log(stringAnagramMap);
+  let stringAnagramMap = {};
+  let result = 0;
+
+  for (let i = 0; i <= s.length; i++) {
+    for (let j = 0; j < i; j++) {
+      const sliceIJ = sortString(s.slice(j, i));
+      if (i !== j && sliceIJ !== "") {
+        if (!!stringAnagramMap[sliceIJ]) {
+          stringAnagramMap[sliceIJ] += 1;
+          result += stringAnagramMap[sliceIJ] - 1;
+          //console.log(result)
+        } else if (i !== j) {
+          stringAnagramMap[sliceIJ] = 1;
         }
+      }
+
+      //console.log(stringAnagramMap);
     }
-    
-    // console.log(stringAnagramMap);
-    return result;
+  }
+
+  // console.log(stringAnagramMap);
+  return result;
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const q = parseInt(readLine(), 10);
+  const q = parseInt(readLine(), 10);
 
-    for (let qItr = 0; qItr < q; qItr++) {
-        const s = readLine();
+  for (let qItr = 0; qItr < q; qItr++) {
+    const s = readLine();
 
-        let result = sherlockAndAnagrams(s);
+    let result = sherlockAndAnagrams(s);
 
-        ws.write(result + "\n");
-    }
+    ws.write(result + "\n");
+  }
 
-    ws.end();
+  ws.end();
 }
---
+++ Dictionaries and Hashmaps (HackerRank)/Sherlock and Anagrams/description.md
@@ -0,0 +1,53 @@
+# Sherlock and Anagrams
+
+Two strings are anagrams of each other if the letters of one string can be rearranged to form the other string. Given a string, find the number of pairs of substrings of the string that are anagrams of each other.
+
+Example
+
+s = mom
+The list of all anagrammatic pairs [m,m],[mo,om] is  at positions [[0],[2],[[0,1],[1,2]] respectively.
+
+These do not share a substring.
+ 
+### Function Description
+
+Complete the function sherlockAndAnagrams in the editor below.
+
+sherlockAndAnagrams has the following parameter(s):
+
+* string s: a string
+
+### Returns
+
+* int: the number of unordered anagrammatic pairs of substrings in s
+
+### Input Format
+
+The first line contains an integer , the number of queries.
+Each of the next  lines contains a string  to analyze.
+
+### Constraints
+
+* 1 <= q <= 10
+* 2 <= length of s <= 100
+* s contains only lowercase letters in the range ascii[a-z].
+
+### Sample Input 
+
+> 2<br>
+  abba<br>
+  abcd
+
+### Sample Output
+
+> 4<br>
+  0
+
+### Explanation
+
+The list of all anagrammatic pairs is [a,a],[ab,ba],[b,b] and [abb,bba] at positions [[0],[3]],[[0,1],[2,3]],[[1],[2]] and [[0,1,2],[1,2,3]] respectively.
+
+No anagrammatic pairs exist in the second query as no character repeats.
+
+## Resolution
+
