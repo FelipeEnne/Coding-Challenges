a183f40788bd55076efbfe8ca5b77c3752046f19
Felipe Enne <felipeenne@gmail.com>
2022-12-06 20:09:13 -0300

    eslint Abbreviation

--- Dynamic Programming (HackerRank)/Abbreviation.js
+++ Dynamic Programming (HackerRank)/Abbreviation.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -32,43 +32,47 @@ function readLine() {
  */
 
 function abbreviation(a, b) {
-    let dp=Array(a.length+1).fill(0);
-    
-    for(let i=0;i<=a.length;i++){
-        dp[i]=Array(b.length+1).fill(0);
-    }
-    
-    dp[0][0]=1;
-    
-    // console.log(dp)
-    
-     for(let i=0;i<a.length;i++){
-       for(let j=0;j<=b.length;j++){
-             if(dp[i][j]===0) continue;
-             if(j<b.length && a[i].toUpperCase()===b[j]){dp[i+1][j+1]=1;}
-             if(a[i]!==a[i].toUpperCase()){dp[i+1][j]=1;}
-          }
+  let dp = Array(a.length + 1).fill(0);
+
+  for (let i = 0; i <= a.length; i++) {
+    dp[i] = Array(b.length + 1).fill(0);
+  }
+
+  dp[0][0] = 1;
+
+  // console.log(dp)
+
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j <= b.length; j++) {
+      if (dp[i][j] === 0) continue;
+      if (j < b.length && a[i].toUpperCase() === b[j]) {
+        dp[i + 1][j + 1] = 1;
       }
-    
-    console.log(dp)
-    
-    return dp[a.length][b.length]?"YES":"NO";
+      if (a[i] !== a[i].toUpperCase()) {
+        dp[i + 1][j] = 1;
+      }
+    }
+  }
+
+  console.log(dp);
+
+  return dp[a.length][b.length] ? "YES" : "NO";
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const q = parseInt(readLine().trim(), 10);
+  const q = parseInt(readLine().trim(), 10);
 
-    for (let qItr = 0; qItr < q; qItr++) {
-        const a = readLine();
+  for (let qItr = 0; qItr < q; qItr++) {
+    const a = readLine();
 
-        const b = readLine();
+    const b = readLine();
 
-        const result = abbreviation(a, b);
+    const result = abbreviation(a, b);
 
-        ws.write(result + '\n');
-    }
+    ws.write(result + "\n");
+  }
 
-    ws.end();
+  ws.end();
 }
