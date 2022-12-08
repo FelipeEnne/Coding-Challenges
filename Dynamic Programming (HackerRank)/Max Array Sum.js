d67bab3e0ee45d71f4bc465dbfdc7a45a3a18659
Felipe Enne <felipeenne@gmail.com>
2022-12-08 17:12:01 -0300

    eslint Max Array Sum

--- Dynamic Programming (HackerRank)/Max Array Sum.js
+++ Dynamic Programming (HackerRank)/Max Array Sum.js
@@ -1,55 +1,61 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
+process.stdin.on("end", function () {
+  inputString = inputString
+    .replace(/\s*$/, "")
+    .split("\n")
+    .map((str) => str.replace(/\s*$/, ""));
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 // Complete the maxSubsetSum function below.
 function maxSubsetSum(arr) {
+  let maxArr = [];
 
-    let maxArr = [];
-     
-    maxArr.push(0);
-    maxArr.push(arr[0]);
-    
-    for(let i=2; i<=arr.length; ++i){
-        let findMax = Math.max(maxArr[i-2] + arr[i-1],maxArr[i-1],arr[i-1])
-        maxArr.push(findMax);
-    }
+  maxArr.push(0);
+  maxArr.push(arr[0]);
 
-    return maxArr.pop();
+  for (let i = 2; i <= arr.length; ++i) {
+    let findMax = Math.max(
+      maxArr[i - 2] + arr[i - 1],
+      maxArr[i - 1],
+      arr[i - 1]
+    );
+    maxArr.push(findMax);
+  }
+
+  return maxArr.pop();
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const n = parseInt(readLine(), 10);
+  const n = parseInt(readLine(), 10);
 
-    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+  const arr = readLine()
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
 
-    const res = maxSubsetSum(arr);
+  const res = maxSubsetSum(arr);
 
-    ws.write(res + '\n');
+  ws.write(res + "\n");
 
-    ws.end();
+  ws.end();
 }
