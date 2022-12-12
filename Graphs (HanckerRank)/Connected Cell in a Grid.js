2bbe12c8936fdb122cb1f6ddcf29bd10cf5852f3
Felipe Enne <felipeenne@gmail.com>
2022-12-12 18:36:29 -0300

    eslint Connected Cell in a Grid

--- Graphs (HanckerRank)/Connected Cell in a Grid.js
+++ Graphs (HanckerRank)/Connected Cell in a Grid.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -30,71 +30,81 @@ function readLine() {
  */
 
 function maxRegion(grid) {
-    // Write your code here
-    let max = 0;
+  // Write your code here
+  let max = 0;
 
-    const memo = new Set();
+  const memo = new Set();
 
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[0].length; j++) {
-            if(grid[i][j] > 0) {
-                //console.log('exploring: ', i,j, max );
-                const size = explore(grid, i, j, memo);
-                if(size > max) max = size;
-            }
-        }
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] > 0) {
+        //console.log('exploring: ', i,j, max );
+        const size = explore(grid, i, j, memo);
+        if (size > max) max = size;
+      }
     }
+  }
 
-    return max;
+  return max;
 }
 
-
 const explore = (grid, r, c, memo) => {
-  if(r < 0 || r >= grid.length) return 0;
-  if(c < 0 || c >= grid[0].length) return 0;
-  
+  if (r < 0 || r >= grid.length) return 0;
+  if (c < 0 || c >= grid[0].length) return 0;
+
   const key = `${r}-${c}`;
 
-  if(memo.has(key)) {
+  if (memo.has(key)) {
     return 0;
   }
 
   memo.add(key);
 
-  if(grid[r][c] === 0) return 0;
+  if (grid[r][c] === 0) return 0;
 
   let size = 1;
 
-  const steps = [[0,1],[1,0],[0,-1],[-1,0],[-1,-1],[-1,1],[1,1],[1,-1]];
+  const steps = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0],
+    [-1, -1],
+    [-1, 1],
+    [1, 1],
+    [1, -1],
+  ];
 
-  for(let i = 0; i < steps.length; i++) {
+  for (let i = 0; i < steps.length; i++) {
     const res = explore(grid, r + steps[i][0], c + steps[i][1], memo);
 
-    if(res > 0) {
+    if (res > 0) {
       size += res;
     }
   }
 
-  return size
-
-}
+  return size;
+};
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const n = parseInt(readLine().trim(), 10);
+  const n = parseInt(readLine().trim(), 10);
 
-    const m = parseInt(readLine().trim(), 10);
+  const m = parseInt(readLine().trim(), 10);
 
-    let grid = Array(n);
+  let grid = Array(n);
 
-    for (let i = 0; i < n; i++) {
-        grid[i] = readLine().replace(/\s+$/g, '').split(' ').map(gridTemp => parseInt(gridTemp, 10));
-    }
+  for (let i = 0; i < n; i++) {
+    grid[i] = readLine()
+      .replace(/\s+$/g, "")
+      .split(" ")
+      .map((gridTemp) => parseInt(gridTemp, 10));
+  }
 
-    const res = maxRegion(grid);
+  const res = maxRegion(grid);
 
-    ws.write(res + '\n');
+  ws.write(res + "\n");
 
-    ws.end();
+  ws.end();
 }
