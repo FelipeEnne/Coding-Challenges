4de566415384254506148b1c6a9d6e1199bab054
Felipe Enne <felipeenne@gmail.com>
2022-12-15 20:29:41 -0300

    eslint Greedy Florist

--- Graphs (HanckerRank)/Roads and Libraries.js
+++ Graphs (HanckerRank)/Roads and Libraries.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -34,71 +34,73 @@ function readLine() {
  */
 // Create Adjacency List
 function adjacencyList(cities) {
-    const map = new Map();
-    cities.forEach(c => {
-        const [c1, c2] = c;
-        if (map.has(c1)) map.get(c1).cities.push(c2);
-        else map.set(c1, {cities: [c2], visited: false});
-        if (map.has(c2)) map.get(c2).cities.push(c1);
-        else map.set(c2, {cities: [c1], visited: false});
-    });
-    return map;
+  const map = new Map();
+  cities.forEach((c) => {
+    const [c1, c2] = c;
+    if (map.has(c1)) map.get(c1).cities.push(c2);
+    else map.set(c1, { cities: [c2], visited: false });
+    if (map.has(c2)) map.get(c2).cities.push(c1);
+    else map.set(c2, { cities: [c1], visited: false });
+  });
+  return map;
 }
 
 // Depth-First Search
 function dfs(vals, map, roads = 0) {
-    if (vals.visited) return roads;
-    vals.visited = true;
-    vals.cities.forEach(v => {
-        roads += dfs(map.get(v), map);
-    });
-    return roads + 1;
+  if (vals.visited) return roads;
+  vals.visited = true;
+  vals.cities.forEach((v) => {
+    roads += dfs(map.get(v), map);
+  });
+  return roads + 1;
 }
 
 function roadsAndLibraries(n, c_lib, c_road, cities) {
-    if(c_road >= c_lib) return c_lib*n;
-    const map = adjacencyList(cities);
-    
-    let libs = 0;
-    let roads = 0;
+  if (c_road >= c_lib) return c_lib * n;
+  const map = adjacencyList(cities);
 
-    for (const vals of map.values()) {
-        if (vals.visited) continue;
-        roads += dfs(vals, map)-1;
-        libs++;
-    }
-    
-    libs += n - map.size;
-    return c_lib * libs + c_road * roads;
-   
+  let libs = 0;
+  let roads = 0;
+
+  for (const vals of map.values()) {
+    if (vals.visited) continue;
+    roads += dfs(vals, map) - 1;
+    libs++;
+  }
+
+  libs += n - map.size;
+  return c_lib * libs + c_road * roads;
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const q = parseInt(readLine().trim(), 10);
+  const q = parseInt(readLine().trim(), 10);
 
-    for (let qItr = 0; qItr < q; qItr++) {
-        const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+  for (let qItr = 0; qItr < q; qItr++) {
+    const firstMultipleInput = readLine().replace(/\s+$/g, "").split(" ");
 
-        const n = parseInt(firstMultipleInput[0], 10);
+    const n = parseInt(firstMultipleInput[0], 10);
 
-        const m = parseInt(firstMultipleInput[1], 10);
+    const m = parseInt(firstMultipleInput[1], 10);
 
-        const c_lib = parseInt(firstMultipleInput[2], 10);
+    const c_lib = parseInt(firstMultipleInput[2], 10);
 
-        const c_road = parseInt(firstMultipleInput[3], 10);
+    const c_road = parseInt(firstMultipleInput[3], 10);
 
-        let cities = Array(m);
+    let cities = Array(m);
 
-        for (let i = 0; i < m; i++) {
-            cities[i] = readLine().replace(/\s+$/g, '').split(' ').map(citiesTemp => parseInt(citiesTemp, 10));
-        }
-
-        const result = roadsAndLibraries(n, c_lib, c_road, cities);
-
-        ws.write(result + '\n');
+    for (let i = 0; i < m; i++) {
+      cities[i] = readLine()
+        .replace(/\s+$/g, "")
+        .split(" ")
+        .map((citiesTemp) => parseInt(citiesTemp, 10));
     }
 
-    ws.end();
+    const result = roadsAndLibraries(n, c_lib, c_road, cities);
+
+    ws.write(result + "\n");
+  }
+
+  ws.end();
 }
--- Greedy Algorithms (HackerRank)/Greedy Florist.js
+++ Greedy Algorithms (HackerRank)/Greedy Florist.js
@@ -1,66 +1,68 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
+process.stdin.on("end", function () {
+  inputString = inputString
+    .replace(/\s*$/, "")
+    .split("\n")
+    .map((str) => str.replace(/\s*$/, ""));
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 // Complete the getMinimumCost function below.
 function getMinimumCost(k, c) {
-    c.sort((a,b) => b-a);
-    const cCount = c.length;
-    // console.log({k,cCount, c});
-    
-    let originalPrice = 0;
-    let cost = 0;
-    
-    for(let i = 0; cCount > i; i++) {
-        if(i-k+1 > 0) {
-            originalPrice = Math.floor(i/k);
-        } 
-        
-        cost += c[i]*(originalPrice + 1);
-        //console.log({k, cost});
-        
+  c.sort((a, b) => b - a);
+  const cCount = c.length;
+  // console.log({k,cCount, c});
+
+  let originalPrice = 0;
+  let cost = 0;
+
+  for (let i = 0; cCount > i; i++) {
+    if (i - k + 1 > 0) {
+      originalPrice = Math.floor(i / k);
     }
-    
-    // console.log({originalPrice});
-    return cost;
+
+    cost += c[i] * (originalPrice + 1);
+    //console.log({k, cost});
+  }
+
+  // console.log({originalPrice});
+  return cost;
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const nk = readLine().split(' ');
+  const nk = readLine().split(" ");
 
-    const n = parseInt(nk[0], 10);
+  const n = parseInt(nk[0], 10);
 
-    const k = parseInt(nk[1], 10);
+  const k = parseInt(nk[1], 10);
 
-    const c = readLine().split(' ').map(cTemp => parseInt(cTemp, 10));
+  const c = readLine()
+    .split(" ")
+    .map((cTemp) => parseInt(cTemp, 10));
 
-    const minimumCost = getMinimumCost(k, c);
+  const minimumCost = getMinimumCost(k, c);
 
-    ws.write(minimumCost + '\n');
+  ws.write(minimumCost + "\n");
 
-    ws.end();
+  ws.end();
 }
