dce7d7591b8399b0d1793ecefdfadf853c170ba9
Felipe Enne <felipeenne@gmail.com>
2022-12-19 20:25:20 -0300

    eslint Max Min

--- Greedy Algorithms (HackerRank)/Max Min.js
+++ Greedy Algorithms (HackerRank)/Max Min.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -32,37 +32,37 @@ function readLine() {
  */
 
 function maxMin(k, arr) {
-    // Write your code here
-    arr.sort((a,b) => a - b);
-    let result = 100000;
-    
-    for(let i = 0; arr.length - k >= i ; i++ ){
-        if(arr[i+k-1] - arr[i] < result) {
-            result = arr[i+k-1] - arr[i];
-        }
-         // console.log({result,i: arr[i], o: arr[i+k-1]})
+  // Write your code here
+  arr.sort((a, b) => a - b);
+  let result = 100000;
+
+  for (let i = 0; arr.length - k >= i; i++) {
+    if (arr[i + k - 1] - arr[i] < result) {
+      result = arr[i + k - 1] - arr[i];
     }
-    // console.log({k, arr, result})
-    return result;
+    // console.log({result,i: arr[i], o: arr[i+k-1]})
+  }
+  // console.log({k, arr, result})
+  return result;
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const n = parseInt(readLine().trim(), 10);
+  const n = parseInt(readLine().trim(), 10);
 
-    const k = parseInt(readLine().trim(), 10);
+  const k = parseInt(readLine().trim(), 10);
 
-    let arr = [];
+  let arr = [];
 
-    for (let i = 0; i < n; i++) {
-        const arrItem = parseInt(readLine().trim(), 10);
-        arr.push(arrItem);
-    }
+  for (let i = 0; i < n; i++) {
+    const arrItem = parseInt(readLine().trim(), 10);
+    arr.push(arrItem);
+  }
 
-    const result = maxMin(k, arr);
+  const result = maxMin(k, arr);
 
-    ws.write(result + '\n');
+  ws.write(result + "\n");
 
-    ws.end();
+  ws.end();
 }
