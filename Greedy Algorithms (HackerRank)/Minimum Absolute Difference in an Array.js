a36b678be4911ce60e386f618febf8aa00cb1c1f
Felipe Enne <felipeenne@gmail.com>
2022-12-20 20:46:17 -0300

    eslint Minimum Absolute Difference in an Array

--- Greedy Algorithms (HackerRank)/Minimum Absolute Difference in an Array.js
+++ Greedy Algorithms (HackerRank)/Minimum Absolute Difference in an Array.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -30,32 +30,34 @@ function readLine() {
  */
 
 function minimumAbsoluteDifference(arr) {
-    // Write your code here
-    let result = 1000000000000;
-    
-    const arrSort = arr.sort((a, b) => a - b)
-    
-    console.log(arrSort); 
-    
-    for(let i = 0; arrSort.length-1 > i; i ++){ 
-        if(Math.abs(arrSort[i] - arrSort[i+1]) < result)
-            result = Math.abs(arrSort[i] - arrSort[i+1]);       
-    }
-    
-    return result;
+  // Write your code here
+  let result = 1000000000000;
 
+  const arrSort = arr.sort((a, b) => a - b);
+
+  console.log(arrSort);
+
+  for (let i = 0; arrSort.length - 1 > i; i++) {
+    if (Math.abs(arrSort[i] - arrSort[i + 1]) < result)
+      result = Math.abs(arrSort[i] - arrSort[i + 1]);
+  }
+
+  return result;
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const n = parseInt(readLine().trim(), 10);
+  const n = parseInt(readLine().trim(), 10);
 
-    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
 
-    const result = minimumAbsoluteDifference(arr);
+  const result = minimumAbsoluteDifference(arr);
 
-    ws.write(result + '\n');
+  ws.write(result + "\n");
 
-    ws.end();
+  ws.end();
 }
