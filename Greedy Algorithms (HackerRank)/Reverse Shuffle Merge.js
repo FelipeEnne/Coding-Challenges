a68f20e58b9fbc8e151b672a44ac83ebfe9818fd
Felipe Enne <felipeenne@gmail.com>
2022-12-22 20:38:51 -0300

    eslint Reverse Shuffle Merge

--- Greedy Algorithms (HackerRank)/Reverse Shuffle Merge.js
+++ Greedy Algorithms (HackerRank)/Reverse Shuffle Merge.js
@@ -1,23 +1,23 @@
-'use strict';
+"use strict";
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -29,41 +29,43 @@ function readLine() {
  */
 
 function whatFlavors(cost, money) {
-    // Write your code here
-    // console.log({cost, money})
-    let sum = 10000;
-    let f1 = 0;
-    let f2 = 1;
-    
-    for(let i = 0; cost.length > i ; i++) {
-        for(let j = 1; cost.length > j ; j++) {
-            if(cost[i] + cost[j] == money && i !== j){
-                f1 = i;
-                f2 = j;
-                sum = cost[i] + cost[j];
-            }
-        }
+  // Write your code here
+  // console.log({cost, money})
+  let sum = 10000;
+  let f1 = 0;
+  let f2 = 1;
+
+  for (let i = 0; cost.length > i; i++) {
+    for (let j = 1; cost.length > j; j++) {
+      if (cost[i] + cost[j] == money && i !== j) {
+        f1 = i;
+        f2 = j;
+        sum = cost[i] + cost[j];
+      }
     }
-    
-    // console.log({sum, f1, f2})
-    if(f1 < f2) {
-        console.log(`${f1+1} ${f2+1}`)
-    } else {
-        console.log(`${f2+1} ${f1+1}`)
-    }
-    
+  }
+
+  // console.log({sum, f1, f2})
+  if (f1 < f2) {
+    console.log(`${f1 + 1} ${f2 + 1}`);
+  } else {
+    console.log(`${f2 + 1} ${f1 + 1}`);
+  }
 }
 
 function main() {
-    const t = parseInt(readLine().trim(), 10);
+  const t = parseInt(readLine().trim(), 10);
 
-    for (let tItr = 0; tItr < t; tItr++) {
-        const money = parseInt(readLine().trim(), 10);
+  for (let tItr = 0; tItr < t; tItr++) {
+    const money = parseInt(readLine().trim(), 10);
 
-        const n = parseInt(readLine().trim(), 10);
+    const n = parseInt(readLine().trim(), 10);
 
-        const cost = readLine().replace(/\s+$/g, '').split(' ').map(costTemp => parseInt(costTemp, 10));
+    const cost = readLine()
+      .replace(/\s+$/g, "")
+      .split(" ")
+      .map((costTemp) => parseInt(costTemp, 10));
 
-        whatFlavors(cost, money);
-    }
+    whatFlavors(cost, money);
+  }
 }
