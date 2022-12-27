597542ca30988aa53953355e4e6725ddf3f937f4
Felipe Enne <felipeenne@gmail.com>
2022-12-27 07:11:07 -0300

    eslint Marc´s Cakewalk

--- HackerRank/Marc's Cakewalk.js
+++ HackerRank/Marc's Cakewalk.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -30,20 +30,22 @@ function readLine() {
  */
 
 function marcsCakewalk(calorie) {
-    // Write your code here
-
+  // Write your code here
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const n = parseInt(readLine().trim(), 10);
+  const n = parseInt(readLine().trim(), 10);
 
-    const calorie = readLine().replace(/\s+$/g, '').split(' ').map(calorieTemp => parseInt(calorieTemp, 10));
+  const calorie = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((calorieTemp) => parseInt(calorieTemp, 10));
 
-    const result = marcsCakewalk(calorie);
+  const result = marcsCakewalk(calorie);
 
-    ws.write(result + '\n');
+  ws.write(result + "\n");
 
-    ws.end();
+  ws.end();
 }
