22b9b751f152903d893240c0815d32021f4af793
Felipe Enne <felipeenne@gmail.com>
2022-12-30 18:07:41 -0300

    estlint add-two-numbers

--- LeetCode/Add Two Numbers/add-two-numbers.js
+++ LeetCode/Add Two Numbers/add-two-numbers.js
@@ -10,34 +10,33 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {
-    let dummy = new ListNode(0);
-    let cur = dummy;
-    let carry = 0;
-    
-    while(l1 != null || l2 != null || carry != 0) {
-        sum = 0;
-        
-        if(l1 != null){
-            sum += l1.val
-            l1 = l1.next
-        }
-        
-        if(l2 != null){
-            sum += l2.val
-            l2 = l2.next
-        }
-        
-        if(carry != 0) {
-            sum += carry;
-        }
-        
-        carry = Math.floor(sum/10)
-        let remainer = sum % 10
-        
-        cur.next = new ListNode(remainer)
-        cur = cur.next
-        
+var addTwoNumbers = function (l1, l2) {
+  let dummy = new ListNode(0);
+  let cur = dummy;
+  let carry = 0;
+
+  while (l1 != null || l2 != null || carry != 0) {
+    sum = 0;
+
+    if (l1 != null) {
+      sum += l1.val;
+      l1 = l1.next;
     }
-    return dummy.next
+
+    if (l2 != null) {
+      sum += l2.val;
+      l2 = l2.next;
+    }
+
+    if (carry != 0) {
+      sum += carry;
+    }
+
+    carry = Math.floor(sum / 10);
+    let remainer = sum % 10;
+
+    cur.next = new ListNode(remainer);
+    cur = cur.next;
+  }
+  return dummy.next;
 };
