f8c4d5f557ae22ceee8b817c6e4dd47aff0772e3
Felipe Enne <felipeenne@gmail.com>
2023-01-02 17:04:52 -0300

    estilint cloneGraph

--- LeetCode/Clone Graph/cloneGraph.js
+++ LeetCode/Clone Graph/cloneGraph.js
@@ -3,29 +3,28 @@
  * @return {Node}
  */
 
-
 function Node(val, neighbors) {
-    this.val = val === undefined ? 0 : val;
-    this.neighbors = neighbors === undefined ? [] : neighbors;
-};
+  this.val = val === undefined ? 0 : val;
+  this.neighbors = neighbors === undefined ? [] : neighbors;
+}
 
-var cloneGraph = function(node) {
-    if (node === null) {
-      return null;
-    }
-    const map = new Map();
-      
-    const clone = root => {
-      if (!map.has(root.val)) {
+var cloneGraph = function (node) {
+  if (node === null) {
+    return null;
+  }
+  const map = new Map();
 
-        map.set(root.val, new Node(root.val));
+  const clone = (root) => {
+    if (!map.has(root.val)) {
+      map.set(root.val, new Node(root.val));
 
-        map.get(root.val).neighbors = root.neighbors.map((n) => {clone(n)});
-      }
-
-      return map.get(root.val);
-      
+      map.get(root.val).neighbors = root.neighbors.map((n) => {
+        clone(n);
+      });
     }
 
-    return clone(node);
+    return map.get(root.val);
   };
+
+  return clone(node);
+};
