67e078d9e8c15b1afb251e83e9df17b50afa40cf
Felipe Enne <felipeenne@gmail.com>
2023-01-03 22:08:06 -0300

    estilint coin-change

--- LeetCode/Coin Change/coin-change.js
+++ LeetCode/Coin Change/coin-change.js
@@ -3,30 +3,26 @@
  * @param {number} amount
  * @return {number}
  */
-var coinChange = function(coins, amount) {
-    
-    const memo = new Map();
-    
-    const permute = left => {
-        if(memo.has(left)) return memo.get(left);
-        
-        if(left == 0) return 0;
-        
-        let min = 100000;
-        //console.log(min)
-        
-        for(let coin of coins) {
-            //console.log(memo)
-            if(left-coin >= 0) {
-                min = Math.min(min, permute(left-coin));
-            }
-        }
-        memo.set(left, min + 1);
-        return min + 1;
-        
+var coinChange = function (coins, amount) {
+  const memo = new Map();
+
+  const permute = (left) => {
+    if (memo.has(left)) return memo.get(left);
+
+    if (left == 0) return 0;
+
+    let min = 100000;
+    //console.log(min)
+
+    for (let coin of coins) {
+      //console.log(memo)
+      if (left - coin >= 0) {
+        min = Math.min(min, permute(left - coin));
+      }
     }
-    const result = permute(amount);
-    return result >= 100000 ? -1 : result;
-    
-    
+    memo.set(left, min + 1);
+    return min + 1;
+  };
+  const result = permute(amount);
+  return result >= 100000 ? -1 : result;
 };
