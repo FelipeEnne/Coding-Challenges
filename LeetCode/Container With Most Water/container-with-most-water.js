c855a06b3b1be2895dae5e424852423fa7611c6a
Felipe Enne <felipeenne@gmail.com>
2023-01-04 21:06:49 -0300

    estilint container-with-most-water

--- LeetCode/Container With Most Water/container-with-most-water.js
+++ LeetCode/Container With Most Water/container-with-most-water.js
@@ -2,22 +2,22 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(height) {
-    let first = 0
-    let last = height.length -1
-    let result = -1
-    
-    while(last > first){
-        let area = (last-first)*(Math.min(height[first], height[last]));
-        if(area > result){
-            result = area
-        }
-        
-        if(height[first] < height[last]){
-            first++;
-        } else {
-            last--;
-        }
+var maxArea = function (height) {
+  let first = 0;
+  let last = height.length - 1;
+  let result = -1;
+
+  while (last > first) {
+    let area = (last - first) * Math.min(height[first], height[last]);
+    if (area > result) {
+      result = area;
     }
-    return result;
+
+    if (height[first] < height[last]) {
+      first++;
+    } else {
+      last--;
+    }
+  }
+  return result;
 };
