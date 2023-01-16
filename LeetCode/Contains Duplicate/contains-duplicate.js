4da60680cb9f3972379d6c189f1c9fcb11b9d490
FelipeEnne <felipeenne@gmail.com>
2023-01-16 19:57:51 -0300

    add contains-duplicate

--- LeetCode/Contains Duplicate/contains-duplicate.js
+++ LeetCode/Contains Duplicate/contains-duplicate.js
@@ -2,16 +2,15 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-    let map = new Map();
-    
-    for(let i = 0; nums.length > i; ++i) {
-       if (!map.has(nums[i])) {
-           map.set(nums[i], nums[i])
-       } else {
-           return true;
-       }
-        
+var containsDuplicate = function (nums) {
+  let map = new Map();
+
+  for (let i = 0; nums.length > i; ++i) {
+    if (!map.has(nums[i])) {
+      map.set(nums[i], nums[i]);
+    } else {
+      return true;
     }
-    return false;
+  }
+  return false;
 };
