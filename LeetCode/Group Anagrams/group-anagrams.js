dc3ffdecbf56334429c587e431bf4b6a7f48d3ae
FelipeEnne <felipeenne@gmail.com>
2023-01-21 20:36:19 -0300

    estlint grorp-anagrams

--- LeetCode/Group Anagrams/group-anagrams.js
+++ LeetCode/Group Anagrams/group-anagrams.js
@@ -2,16 +2,16 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function(strs) {
-    let map = new Map();
-    
-    for (const str of strs) {
-        const sorted = [...str].sort().join("");
-        if(map.has(sorted)){
-            map.get(sorted).push(str);
-        }else{
-           map.set(sorted, [str]);
-        }
+var groupAnagrams = function (strs) {
+  let map = new Map();
+
+  for (const str of strs) {
+    const sorted = [...str].sort().join("");
+    if (map.has(sorted)) {
+      map.get(sorted).push(str);
+    } else {
+      map.set(sorted, [str]);
     }
-    return [...map.values()];
+  }
+  return [...map.values()];
 };
