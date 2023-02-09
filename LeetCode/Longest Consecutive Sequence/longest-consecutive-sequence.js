8225cbd0338d51e2dd3cd7ce430afe1f58fd39d4
FelipeEnne <felipeenne@gmail.com>
2023-02-09 07:17:13 -0300

    estlint longest-consecutive-sequence

--- LeetCode/Longest Consecutive Sequence/longest-consecutive-sequence.js
+++ LeetCode/Longest Consecutive Sequence/longest-consecutive-sequence.js
@@ -2,28 +2,27 @@
  * @param {number[]} nums
  * @return {number}
  */
-function onlyUnique(value, index, self) { 
-    return self.indexOf(value) === index;
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
 }
 
-var longestConsecutive = function(nums) {
-    nums.sort((a, b) =>  a - b)
-    count = 0;
-    nums = nums.filter( onlyUnique )
-    
-    //console.log(nums)
-    
-    for(let i = 0; nums.length > i; i++) {
-        currentCount = 1;
-        currentIndex = i;
-        
-        while(nums[currentIndex]+1 == nums[currentIndex+1] ) {
-            currentCount += 1;
-            currentIndex += 1;
-        }
-        count = Math.max(count, currentCount);
+var longestConsecutive = function (nums) {
+  nums.sort((a, b) => a - b);
+  count = 0;
+  nums = nums.filter(onlyUnique);
+
+  //console.log(nums)
+
+  for (let i = 0; nums.length > i; i++) {
+    currentCount = 1;
+    currentIndex = i;
+
+    while (nums[currentIndex] + 1 == nums[currentIndex + 1]) {
+      currentCount += 1;
+      currentIndex += 1;
     }
-    
-    
-    return count
+    count = Math.max(count, currentCount);
+  }
+
+  return count;
 };
