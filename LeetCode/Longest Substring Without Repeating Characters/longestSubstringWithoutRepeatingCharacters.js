b51a76386b2124857d03f350c8d97eae18127b4d
FelipeEnne <felipeenne@gmail.com>
2023-02-10 07:31:55 -0300

    estlint longestSubstringWithoutRepeatingCharacters

--- LeetCode/Longest Substring Without Repeating Characters/longestSubstringWithoutRepeatingCharacters.js
+++ LeetCode/Longest Substring Without Repeating Characters/longestSubstringWithoutRepeatingCharacters.js
@@ -2,25 +2,22 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-    if(s == "") return 0
-    
-    let count = 1;
-    let currentCount = 0;
-    let map =  new Map();
-    
-    for(let i = 0; s.length > i; i++ ){
-        
-        
-        if(map.get(s[i]) >= currentCount ) {
-            currentCount = map.get(s[i])+1
-        }
+var lengthOfLongestSubstring = function (s) {
+  if (s == "") return 0;
 
-        
-        map.set(s[i], i)
-        
-        count = Math.max(i-currentCount+1, count)
+  let count = 1;
+  let currentCount = 0;
+  let map = new Map();
+
+  for (let i = 0; s.length > i; i++) {
+    if (map.get(s[i]) >= currentCount) {
+      currentCount = map.get(s[i]) + 1;
     }
-    // console.log(map)
-    return count
+
+    map.set(s[i], i);
+
+    count = Math.max(i - currentCount + 1, count);
+  }
+  // console.log(map)
+  return count;
 };
