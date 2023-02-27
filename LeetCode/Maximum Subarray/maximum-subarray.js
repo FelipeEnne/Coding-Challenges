675788cce36045bb080c40228fc6b51096ab9ccb
FelipeEnne <felipeenne@gmail.com>
2023-02-27 07:12:58 -0300

    estlint maximum-subarray

--- LeetCode/Maximum Subarray/maximum-subarray.js
+++ LeetCode/Maximum Subarray/maximum-subarray.js
@@ -2,28 +2,28 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray1 = function(nums) {
-    let result  = Number.NEGATIVE_INFINITY
-    let last = 0
-    
-    for(let i = 0; nums.length > i; i++){
-       last = nums[i];
-        let j = i+1;
-        if(nums[i] > result){
-            result = nums[i]
-        }
-        while(j < nums.length){
-            let adding =  last + nums[j];
-            last += nums[j];
-            if(adding > result){
-                result = adding
-            }
-            //console.log(adding)
-            j++;
-        }
-        //console.log(result)
+var maxSubArray1 = function (nums) {
+  let result = Number.NEGATIVE_INFINITY;
+  let last = 0;
+
+  for (let i = 0; nums.length > i; i++) {
+    last = nums[i];
+    let j = i + 1;
+    if (nums[i] > result) {
+      result = nums[i];
     }
-    return result
+    while (j < nums.length) {
+      let adding = last + nums[j];
+      last += nums[j];
+      if (adding > result) {
+        result = adding;
+      }
+      //console.log(adding)
+      j++;
+    }
+    //console.log(result)
+  }
+  return result;
 };
 
 // -------------------------------
@@ -32,11 +32,10 @@ var maxSubArray1 = function(nums) {
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
-    
-    for(let i = 1; nums.length > i; i++){
-        nums[i] = Math.max(nums[i], nums[i]+nums[i-1] );    
-    }
-    
-    return Math.max(...nums)
+var maxSubArray = function (nums) {
+  for (let i = 1; nums.length > i; i++) {
+    nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+  }
+
+  return Math.max(...nums);
 };
